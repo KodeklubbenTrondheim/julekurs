5a4402a7db5ab85d89eaa3c5af11846f493e9018
Niklas Molnes Hole <nikasmh@hotmail.com>
2021-09-30 01:25:18 +0200

    Bruke react router og flytte demo til egen side

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import { useState } from 'react'
 import styled from 'styled-components'
-import create from 'zustand'
-import Editor from '@monaco-editor/react'
-import { runCode, setOptions } from 'client-side-python-runner'
-import { useEffect } from 'react'
-
-const useStore = create((set) => ({
-  code: '# Her er det litt pythonkode\n\na = 1\nprint(a + 2, "test")\n',
-  setCode: (code) => set(() => ({ code })),
-  log: [],
-  addLog: (content, error = false) => set((state) => ({ log: [{ content, error }, ...state.log] })),
-}))
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { Demo } from './pages/demo'
+import { Prosjekt } from './pages/project'
 
 const Container = styled.div`
   background-color: #282c34;
@@ -28,70 +19,33 @@ const Header = styled.div`
   color: white;
 `
 
-const Output = styled.div`
-  white-space: pre;
-  background-color: #0002;
-  padding: 10px 20px;
-
-  :empty {
-    display: none;
-  }
-`
-
-const Button = styled.button`
-  background-color: #3b82f6;
-  color: white;
-  border: none;
-  padding: 10px 16px;
-  border-radius: 4px;
-  margin: 10px;
-  cursor: pointer;
-`
-
 function App() {
-  const code = useStore((state) => state.code)
-  const setCode = useStore((state) => state.setCode)
-  const log = useStore((state) => state.log)
-  const addLog = useStore((state) => state.addLog)
-  const [loadingPython, setLoadingPython] = useState(false)
-
-  useEffect(() => {
-    setOptions({
-      output: (item) => addLog(item),
-      error: (item) => addLog(item, true),
-      onLoading: () => setLoadingPython(true),
-      onLoaded: () => setLoadingPython(false),
-    })
-  }, [addLog])
-
   return (
-    <Container>
-      <Header>
-        <h1>Julekortverkstedet</h1>
-        <p>Lyst til å lage julekort? Da har du kommet til riktig sted!</p>
-        <Editor
-          height="240px"
-          width="640px"
-          theme="vs-dark"
-          defaultLanguage="python"
-          value={code}
-          onChange={(value) => setCode(value)}
-        />
-        <Button onClick={() => runCode(code)}>Kjør koden!</Button>
-        {loadingPython && <h3>Laster inn Python ... Det kan ta noen sekunder</h3>}
-        {log.length > 0 && <h3>Dette ble printet:</h3>}
-        <Output>
-          {log.map(({ content, error }) => {
-            if (error) {
-              return <span style={{ color: 'red' }}>{content.message + '\n'}</span>
-            }
-            return content
-          })}
-        </Output>
-        <h3>Python-koden:</h3>
-        <Output>{code}</Output>
-      </Header>
-    </Container>
+    <Router>
+      <Container>
+        <Header>
+          <h1>Julekortverkstedet</h1>
+          <p>Lyst til å lage julekort? Da har du kommet til riktig sted!</p>
+          <Switch>
+            <Route path="/" exact>
+              <Link to="/demo">Gå til demosiden</Link>
+              <Link to="/project">Gå til prosjekt-siden</Link>
+              Her er det ingenting :) Prøv å gå til demosiden
+            </Route>
+            <Route path="/demo">
+              <Link to="/">Gå tilbake til hjem</Link>
+              <h2>Demo</h2>
+              <Demo />
+            </Route>
+            <Route path="/project">
+              <Link to="/">Gå tilbake til hjem</Link>
+              <h2>Prosjekt</h2>
+              <Prosjekt />
+            </Route>
+          </Switch>
+        </Header>
+      </Container>
+    </Router>
   )
 }
 
---
+++ src/pages/project/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background-color: #282c34;
+  text-align: center;
+  padding: 0 2rem 2rem;
+`
+
+export function Prosjekt() {
+  return <Container>Kode her</Container>
+}
