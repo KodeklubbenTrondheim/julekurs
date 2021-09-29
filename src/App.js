6b6e312cff9bb47a92813c56897ca2d6f39852c8
Niklas Molnes Hole <nikasmh@hotmail.com>
2021-09-29 23:38:45 +0200

    Rydde litt opp i template-koden

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import logo from './logo.svg'
-import './App.css'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  text-align: center;
+`
+
+const Header = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <h1>Julekortverkstedet</h1>
+        <p>Lyst til å lage julekort? Da har du kommet til riktig sted!</p>
+      </Header>
+    </Container>
   )
 }
 
