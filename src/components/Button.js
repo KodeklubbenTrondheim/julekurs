697398a2b9bf0e1f1a01a8958ba228ea8d35eada
Niklas Molnes Hole <nikasmh@hotmail.com>
2021-11-02 21:28:38 +0100

    Flere innstillinger på knapp

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,27 +3,27 @@ import styled from 'styled-components'
 export const Button = styled.button`
   border: none;
   border-radius: 4px;
-  background-color: #666;
-  color: #fff;
-  padding: 8px 12px;
+  background-color: ${(props) => props.$color || '#666'};
+  color: ${(props) => props.$textColor || '#fff'};
+  padding: 0.6em 1em;
   cursor: pointer;
-  font-size: 16px;
+  font-size: ${(props) => props.$size || '16px'};
 
   :hover {
-    background-color: #444;
+    background-color: ${(props) => props.$hoverColor || '#444'};
   }
 `
 
 export const LinkButton = styled.a`
   border: none;
   border-radius: 4px;
-  background-color: #666;
-  color: #fff;
-  padding: 8px 12px;
+  background-color: ${(props) => props.$color || '#666'};
+  color: ${(props) => props.$textColor || '#fff'};
+  padding: 0.6em 1em;
   cursor: pointer;
-  font-size: 16px;
+  font-size: ${(props) => props.$size || '16px'};
 
   :hover {
-    background-color: #444;
+    background-color: ${(props) => props.$hoverColor || '#444'};
   }
 `
