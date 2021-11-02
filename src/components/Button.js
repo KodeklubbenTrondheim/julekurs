e0557f824a0229edc258fc155e8a165d87c52bfd
Niklas Molnes Hole <nikasmh@hotmail.com>
2021-11-02 21:57:20 +0100

    Gjør knappen litt smoothere

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,29 +1,33 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { CSSShadows } from '../constants'
 
-export const Button = styled.button`
+const ButtonStyles = css`
   border: none;
   border-radius: 4px;
   background-color: ${(props) => props.$color || '#666'};
   color: ${(props) => props.$textColor || '#fff'};
   padding: 0.6em 1em;
   cursor: pointer;
+  user-select: none;
   font-size: ${(props) => props.$size || '16px'};
+  ${CSSShadows.large}
+
+  transition-duration: 0.1s;
+  transition-property: background box-shadow;
 
   :hover {
     background-color: ${(props) => props.$hoverColor || '#444'};
+  }
+
+  :active {
+    ${CSSShadows.small}
   }
 `
 
-export const LinkButton = styled.a`
-  border: none;
-  border-radius: 4px;
-  background-color: ${(props) => props.$color || '#666'};
-  color: ${(props) => props.$textColor || '#fff'};
-  padding: 0.6em 1em;
-  cursor: pointer;
-  font-size: ${(props) => props.$size || '16px'};
+export const Button = styled.button`
+  ${ButtonStyles}
+`
 
-  :hover {
-    background-color: ${(props) => props.$hoverColor || '#444'};
-  }
+export const LinkButton = styled.a`
+  ${ButtonStyles}
 `
